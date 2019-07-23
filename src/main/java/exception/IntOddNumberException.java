dfd4f2730dfdbc43290ffc2ba144a05c68a81ef2
i-kovalyshyn <ikovalyshyn@gmail.com>
2019-07-23 11:25:30 +0300

    made package-private

--- src/main/java/exception/IntOddNumberException.java
+++ src/main/java/exception/IntOddNumberException.java
@@ -1,7 +1,7 @@
 package main.java.exception;
 
 public class IntOddNumberException extends Exception {
-    public IntOddNumberException (String msg){
+    IntOddNumberException(String msg){
         super(msg);
     }
 }
