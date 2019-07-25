13f7505e93e7ca8593b25a3eab3fe2fd1c6ee017
i-kovalyshyn <ikovalyshyn@gmail.com>
2019-07-25 15:06:35 +0300

    two same instances were remove

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        CheckOut intNumber = new CheckOut();
+        CheckOut object = new CheckOut();
         System.out.print("Enter the text to check for the palindrome: ");
         String text = scanner.nextLine();
         try {
@@ -17,13 +17,12 @@ public class Main {
             System.out.println(e.getMessage());
         }
         try {
-            intNumber.randomNumber();
+            object.randomNumber();
         } catch (IntOddNumberException | GreaterThanHundredException e) {
             System.out.println(e.getMessage());
         }
         try {
-            CheckOut random = new CheckOut();
-            random.randomObject();
+            object.randomObject();
         } catch (NullObjectException e) {
             System.out.println(e.getMessage());
         }
